1171d5597f6211e03b83695660bfdc58c6d5d338
Pawel Zabczynski <zabczynskipawe@gmail.com>
2022-01-07 00:36:48 +0100

    introduce int module to be able to use transactor which provided as resource. It make not possible to mix in usint trait.

--- web/src/main/scala/com/pawelzabczynski/InitModule.scala
+++ web/src/main/scala/com/pawelzabczynski/InitModule.scala
@@ -1,3 +1,10 @@
-package com.pawelzabczynski trait InitModule {
+package com.pawelzabczynski
+
+import com.pawelzabczynski.config.ConfigModule
+import com.pawelzabczynski.infrastructure.DataBase
+
+trait InitModule extends ConfigModule {
+
+  lazy val dataBase = new DataBase(config.db)
 
 }
